e3bc5b878244b6a4254408b5dce964e8220a3793
Gaétan MARTIN <gmartin@weborama.com>
2017-07-24 18:19:26 +0200

    try disable action

--- new-project/client/components/Actions.vue
+++ new-project/client/components/Actions.vue
@@ -1,38 +1,30 @@
 <template>
     <div class="actions">
         <div v-for="v, k in actions" >
-          <button @click="sendNewAction(v.ID)">{{v.Name}}</button>
+          <button v-if="!actionUsed[v.ID] || actionUsed[v.ID] < this.$store.state.currentGame.CurrentTurn"
+                  @click="sendNewAction(v.ID, v.Cooldown)">{{v.Name}}</button>
        </div>
     </div>
 </template>
 
 <script>
 import axios from "axios"
+import Vue from 'vue';
 export default {
 
     computed: {
       actions: function() {
         return this.$store.state.actions
       },
+      actionUsed: function() {
+        return {}
+      },
     },
     methods: {
       jsonParse(jsonList) {
           return JSON.parse(jsonList)
       },
-      sendNewPolicy(policy, event){
-        axios.post('http://localhost:8081/ChangePolicy', {
-          ID: Number(policy),
-          Value: Number(event.target.value),
-          PlayerID: this.$store.state.playerProfile.ID,
-          GameID: this.$store.state.currentGame.GameID,
-          })
-          .then(function (response) {
-          }.bind(this))
-          .catch(function (error) {
-            console.log(error);
-          });
-      },
-      sendNewAction(action){
+      sendNewAction(action, cd){
         axios.post('http://localhost:8081/Actions', {
           ID: action,
           Value: 1,
@@ -40,7 +32,10 @@ export default {
           GameID: this.$store.state.currentGame.GameID,
           })
           .then(function (response) {
-          }.bind(this))
+            console.log(this.$store.state.currentGame.CurrentTurn);
+            console.log(cd);
+            Vue.set(this.actionUsed, action, cd + this.$store.state.currentGame.CurrentTurn)
+            }.bind(this))
           .catch(function (error) {
             console.log(error);
           });
